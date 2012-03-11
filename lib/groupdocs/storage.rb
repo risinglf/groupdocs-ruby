7138b1905a5692a9c80125d93c021c6b467b1b46
Alex Rodionov <p0deje@gmail.com>
2012-03-11 16:10:31 +0700

    avoid class << self

--- lib/groupdocs/storage.rb
+++ lib/groupdocs/storage.rb
@@ -5,31 +5,29 @@ require 'groupdocs/storage/package'
 
 module GroupDocs
   module Storage
-    class << self
 
-      #
-      # Return an array of information about user's storage.
-      #
-      # @example
-      #   GroupDocs::Storage.info!
-      #   #=> { total_space: "1024 MB", available_space: "1020 MB", document_credits: 5000, available_credits: 4964 }
-      #
-      # @return [Array]
-      #
-      def info!
-        json = GroupDocs::Api::Request.new do |request|
-          request[:method] = :GET
-          request[:path] = "/storage/#{GroupDocs.client_id}"
-        end.execute!
+    #
+    # Return an array of information about user's storage.
+    #
+    # @example
+    #   GroupDocs::Storage.info!
+    #   #=> { total_space: "1024 MB", available_space: "1020 MB", document_credits: 5000, available_credits: 4964 }
+    #
+    # @return [Array]
+    #
+    def self.info!
+      json = GroupDocs::Api::Request.new do |request|
+        request[:method] = :GET
+        request[:path] = "/storage/#{GroupDocs.client_id}"
+      end.execute!
 
-        {
-          total_space:       "#{json[:result][:total_space] / 1048576} MB",
-          available_space:   "#{json[:result][:avail_space] / 1048576} MB",
-          document_credits:  json[:result][:doc_credits],
-          available_credits: json[:result][:avail_credits]
-        }
-      end
+      {
+        total_space:       "#{json[:result][:total_space] / 1048576} MB",
+        available_space:   "#{json[:result][:avail_space] / 1048576} MB",
+        document_credits:  json[:result][:doc_credits],
+        available_credits: json[:result][:avail_credits]
+      }
+    end
 
-    end # << self
   end # Storage
 end # GroupDocs
