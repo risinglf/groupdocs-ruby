002cd1e0ee42f6ce908434d354d6abd0f9847b30
Alex Rodionov <p0deje@gmail.com>
2012-03-11 17:47:50 +0700

    Document#access_mode! and #access_mode=

--- lib/groupdocs/document.rb
+++ lib/groupdocs/document.rb
@@ -7,10 +7,66 @@ module GroupDocs
     # TODO
     attr_accessor :name
 
+    #
+    # Creates new GroupDocs::Document.
+    #
+    # You're encouraged to create documents directly. Instead, use #to_document
+    # instance method of GroupDocs::Storage::File.
+    #
+    # @raise [ArgumentError] If file is not passed or is not an instance of GroupDocs::Storage::File.
+    #
     def initialize(options = {}, &blk)
       super(options, &blk)
       file.is_a?(GroupDocs::Storage::File) or raise ArgumentError,
-        "You have to pass GroupDocs::Storage::File object: #{file.inspect}"
+        "You have to pass GroupDocs::Storage::File object: #{file.inspect}."
+    end
+
+    #
+    # Returns access mode of document.
+    #
+    # @return [Symbol] One of :private, :restricted or :public access modes.
+    #
+    def access_mode!
+      json = GroupDocs::Api::Request.new do |request|
+        request[:method] = :GET
+        request[:path] = "/doc/#{GroupDocs.client_id}/files/#{file.id}/accessinfo"
+      end.execute!
+
+      parse_access_mode(json[:result][:access])
+    end
+
+    #
+    # Sets access mode of document.
+    #
+    # Please note that even thought it is not "bang" method, it still send requests to API server.
+    #
+    # @param [Symbol] mode One of :private, :restricted or :public access modes.
+    # @return [Symbol] Set access mode.
+    #
+    def access_mode=(mode)
+      GroupDocs::Api::Request.new do |request|
+        request[:method] = :PUT
+        request[:path] = "/doc/#{GroupDocs.client_id}/files/#{file.id}/accessinfo?mode=#{parse_access_mode(mode)}"
+      end.execute!
+    end
+
+    private
+
+    #
+    # @api private
+    #
+    def parse_access_mode(mode)
+      modes =  {
+        private:    0,
+        restricted: 1,
+        public:     2
+      }
+
+      if mode.is_a?(Integer)
+        modes.invert[mode]
+      else
+        modes[mode]
+      end or raise ArgumentError, "Unknown access mode: #{mode.inspect}."
     end
 
   end # Document
--- spec/groupdocs/document_spec.rb
+++ spec/groupdocs/document_spec.rb
@@ -4,4 +4,73 @@ describe GroupDocs::Document do
 
   it_behaves_like GroupDocs::Api::Entity
 
+  subject do
+    file = GroupDocs::Storage::File.new
+    described_class.new(file: file)
+  end
+
+  describe '#initialize' do
+    it 'raises error if file is not specified' do
+      -> { described_class.new }.should raise_error(ArgumentError)
+    end
+
+    it 'raises error if file is not an instance of GroupDocs::Storage::File' do
+      -> { described_class.new(file: '') }.should raise_error(ArgumentError)
+    end
+  end
+
+  context 'attributes' do
+    it { should respond_to(:file)  }
+    it { should respond_to(:file=) }
+  end
+
+  context 'instance methods' do
+    describe '#access_mode!' do
+      it 'returns access mode in human readable presentation' do
+        mock_api_server('{"status": "Ok", "result": {"access": 0 }}')
+        subject.should_receive(:parse_access_mode).with(0).and_return(:private)
+        subject.access_mode!.should == :private
+      end
+    end
+
+    describe '#access_mode=' do
+      it 'sets corresponding access mode' do
+        mock_api_server('{"status": "Ok", "result": {"access": 0 }}')
+        subject.should_receive(:parse_access_mode).with(:private).and_return(0)
+        subject.access_mode = :private
+      end
+    end
+
+    describe '#parse_access_mode' do
+      it 'raise error if mode is unknown' do
+        -> { subject.send(:parse_access_mode, 3) }.should raise_error(ArgumentError)
+        -> { subject.send(:parse_access_mode, :unknown) }.should raise_error(ArgumentError)
+      end
+
+      it 'returns :private if passed mode is 0' do
+        subject.send(:parse_access_mode, 0).should == :private
+      end
+
+      it 'returns :restricted if passed mode is 1' do
+        subject.send(:parse_access_mode, 1).should == :restricted
+      end
+
+      it 'returns :public if passed mode is 2' do
+        subject.send(:parse_access_mode, 2).should == :public
+      end
+
+      it 'returns 0 if passed mode is :private' do
+        subject.send(:parse_access_mode, :private).should == 0
+      end
+
+      it 'returns 1 if passed mode is :restricted' do
+        subject.send(:parse_access_mode, :restricted).should == 1
+      end
+
+      it 'returns 2 if passed mode is :public' do
+        subject.send(:parse_access_mode, :public).should == 2
+      end
+    end
+
+  end
 end
