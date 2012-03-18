cd1cf2f00e7d3db29c059b13765a680f44d5dddd
Alex Rodionov <p0deje@gmail.com>
2012-03-18 21:28:42 +0700

    Job initial work

--- lib/groupdocs/job.rb
+++ lib/groupdocs/job.rb
@@ -1,12 +1,94 @@
 module GroupDocs
   class Job < GroupDocs::Api::Entity
 
+    extend GroupDocs::Api::Sugar::Lookup
+    include GroupDocs::Api::Helpers::Status
+
+    #
+    # Returns array of recent jobs.
+    #
+    # @param [Hash] options Hash of options
+    # @option options [Integer] :page Page to start with
+    # @option options [Integer] :count How many items to list
+    # @param [Hash] access Access credentials
+    # @options access [String] :client_id
+    # @options access [String] :private_key
+    # @return [Array<GroupDocs::Job>]
+    #
+    def self.all!(options = {}, access = {})
+      api = GroupDocs::Api::Request.new do |request|
+        request[:access] = access
+        request[:method] = :GET
+        request[:path] = '/{{client_id}}/jobs'
+      end
+      api.add_params(options)
+      json = api.execute!
+
+      json[:jobs].map do |job|
+        GroupDocs::Job.new(job)
+      end
+    end
+
     # @attr [Integer] id
     attr_accessor :id
-    # @attr [Symbol] status
-    attr_accessor :status
-    # @attr [Array<GroupDocs::Document>] documents
+    # @attr [Array<GroupDocs::Document] documents
     attr_accessor :documents
+
+    #
+    # Coverts passed array of attributes hash to array of GroupDocs::Document.
+    #
+    # @param [Array<Hash>] documents Array of document attributes hashes
+    #
+    def documents=(documents)
+      @documents = documents.map do |document|
+        document.merge!(file: GroupDocs::Storage::File.new(document))
+        GroupDocs::Document.new(document)
+      end
+    end
+
+    #
+    # Returns an array of documents associated to job.
+    #
+    # @param [Hash] access Access credentials
+    # @options access [String] :client_id
+    # @options access [String] :private_key
+    # @return [Array<GroupDocs::Document>]
+    #
+    # @todo receive 404
+    #
+    def documents!(access = {})
+      json = GroupDocs::Api::Request.new do |request|
+        request[:access] = access
+        request[:method] = :GET
+        request[:path] = "/{{client_id}}/jobs/#{id}"
+      end.execute!
+    end
+
+    #
+    # Adds document to job.
+    #
+    # @param [GroupDocs::Document] document
+    # @param [Hash] options
+    # @option options [Array] :output_formats Array of output formats to override
+    # @param [Hash] access Access credentials
+    # @options access [String] :client_id
+    # @options access [String] :private_key
+    # @raise [ArgumentError] If document is not a GroupDocs::Document object
+    #
+    # @todo receive "Document not found"
+    #
+    def add_document!(document, options = {}, access = {})
+      document.is_a?(GroupDocs::Document) or raise ArgumentError,
+        "Document should be GroupDocs::Document object. Received: #{document.inspect}"
+
+      api = GroupDocs::Api::Request.new do |request|
+        request[:access] = access
+        request[:method] = :PUT
+        request[:path] = "/{{client_id}}/jobs/#{id}/files/#{document.file.id}"
+      end
+      api.add_params(options)
+      json = api.execute!
+    end
 
   end # Job
 end # GroupDocs
--- spec/groupdocs/job_spec.rb
+++ spec/groupdocs/job_spec.rb
@@ -3,11 +3,61 @@ require 'spec_helper'
 describe GroupDocs::Job do
 
   it_behaves_like GroupDocs::Api::Entity
+  include_examples GroupDocs::Api::Sugar::Lookup
+  include_examples GroupDocs::Api::Helpers::Status
+
+  describe '.all!' do
+    before(:each) do
+      mock_api_server(load_json('jobs_get'))
+    end
+
+    it 'accepts access credentials hash' do
+      lambda do
+        described_class.all!({}, client_id: 'client_id', private_key: 'private_key')
+      end.should_not raise_error(ArgumentError)
+    end
+
+    it 'accepts options hash' do
+      lambda do
+        described_class.all!(page: 1, count: 2)
+      end.should_not raise_error(ArgumentError)
+    end
+
+    it 'returns an array of GroupDocs::Job objects' do
+      jobs = described_class.all!
+      jobs.should be_an(Array)
+      jobs.each do |job|
+        job.should be_a(GroupDocs::Job)
+      end
+    end
+  end
 
   it { should respond_to(:id)         }
   it { should respond_to(:id=)        }
-  it { should respond_to(:status)     }
-  it { should respond_to(:status=)    }
   it { should respond_to(:documents)  }
   it { should respond_to(:documents=) }
+
+  describe '#documents=' do
+    let(:response) do
+      [{ id: 1, guid: 'fhy9yh94u238dgf', status: 0, outputs: [] },
+       { id: 2, guid: 'ofh9rhy9rfohf9s', status: 2, outputs: [] }]
+    end
+
+    it 'saves documents as array of GroupDocs::Document objects' do
+      subject.documents = response
+      documents = subject.documents
+      documents.should be_an(Array)
+      documents.each do |document|
+        document.should be_a(GroupDocs::Document )
+      end
+    end
+  end
+
+  describe '#documents!' do
+    pending 'Receive 404 response.'
+  end
+
+  describe '#add_document!' do
+    pending 'Receive "Document not found" response'
+  end
 end
