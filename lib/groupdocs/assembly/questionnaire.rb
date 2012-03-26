81fc75dbc223677185ed2b9ad9db73d78394b32e
Alex Rodionov <p0deje@gmail.com>
2012-03-26 17:45:55 +0700

    Implemented GroupDocs::Assembly::Questionnaire#create!

--- lib/groupdocs/assembly/questionnaire.rb
+++ lib/groupdocs/assembly/questionnaire.rb
@@ -2,6 +2,54 @@ module GroupDocs
   module Assembly
     class Questionnaire < GroupDocs::Api::Entity
 
+      require 'groupdocs/assembly/questionnaire/page'
+      require 'groupdocs/assembly/questionnaire/question'
+
+      # @attr [Integer] id
+      attr_accessor :id
+      # @attr [String] name
+      attr_accessor :name
+      # @attr [String] descr
+      attr_accessor :descr
+      # @attr [Array<GroupDocs::Assembly::Questionnaire::Page>] pages
+      attr_accessor :pages
+
+      # Human-readable accessors
+      alias_method :description,  :descr
+      alias_method :description=, :descr=
+
+      #
+      # Adds page to questionnaire.
+      #
+      # @param [GroupDocs::Assembly::Questionnaire::Page] page
+      # @raise [ArgumentError] if page is not GroupDocs::Assembly::Questionnaire::Page object
+      #
+      def add_page(page)
+        page.is_a?(GroupDocs::Assembly::Questionnaire::Page) or raise ArgumentError,
+          "Page should be GroupDocs::Assembly::Questionnaire::Page object, received: #{page.inspect}"
+
+        @pages ||= Array.new
+        @pages << page
+      end
+
+      #
+      # Creates questionnaire.
+      #
+      # @param [Hash] access Access credentials
+      # @option access [String] :client_id
+      # @option access [String] :private_key
+      #
+      def create!(access = {})
+        json = GroupDocs::Api::Request.new do |request|
+          request[:access] = access
+          request[:method] = :POST
+          request[:path] = '/merge/{{client_id}}/questionnaires'
+          request[:request_body] = to_hash
+        end.execute!
+
+        self.id = json[:questionnaire_id]
+      end
+
     end # Questionnaire
   end # Assembly
 end # GroupDocs
--- spec/groupdocs/assembly/questionnaire_spec.rb
+++ spec/groupdocs/assembly/questionnaire_spec.rb
@@ -4,4 +4,42 @@ describe GroupDocs::Assembly::Questionnaire do
 
   it_behaves_like GroupDocs::Api::Entity
 
+  it { should respond_to(:pages)  }
+  it { should respond_to(:pages=) }
+
+  describe '#add_page' do
+    it 'raises error if page is not GroupDocs::Assembly::Questionnaire::Page object' do
+      -> { subject.add_page('Page') }.should raise_error(ArgumentError)
+    end
+
+    it 'adds page to pages instance variable' do
+      page = GroupDocs::Assembly::Questionnaire::Page.new
+      lambda do
+        subject.add_page(page)
+      end.should change(subject, :pages).to([page])
+    end
+  end
+
+  describe '#create!' do
+    before(:each) do
+      mock_api_server(load_json('questionnaire_create'))
+    end
+
+    it 'accepts access credentials hash' do
+      lambda do
+        subject.create!(client_id: 'client_id', private_key: 'private_key')
+      end.should_not raise_error(ArgumentError)
+    end
+
+    it 'uses hashed version of self as request body' do
+      subject.should_receive(:to_hash)
+      subject.create!
+    end
+
+    it 'adds ID of questionnaire from response to self' do
+      lambda do
+        subject.create!
+      end.should change(subject, :id)
+    end
+  end
 end
