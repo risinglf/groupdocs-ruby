3157f8129a9cb366f6b8a2c769a23150220fe688
Alex Rodionov <p0deje@gmail.com>
2012-03-28 23:27:30 +0700

    Updated DataSource

--- lib/groupdocs/assembly/datasource.rb
+++ lib/groupdocs/assembly/datasource.rb
@@ -2,8 +2,50 @@ module GroupDocs
   module Assembly
     class DataSource < GroupDocs::Api::Entity
 
+      require 'groupdocs/assembly/datasource/field'
+
       # @attr [Integer] id
       attr_accessor :id
+      # @attr [Integer] questionnaire_id
+      attr_accessor :questionnaire_id
+      # @attr [Time] created_on
+      attr_accessor :created_on
+      # @attr [Time] modified_on
+      attr_accessor :modified_on
+      # @attr [Array<GroupDocs::Assembly::DataSource::Field>] fields
+      attr_accessor :fields
+
+      #
+      # Converts timestamp which is return by API server to Time object.
+      #
+      # @return [Time]
+      #
+      def created_on
+        Time.at(@created_on)
+      end
+
+      #
+      # Converts timestamp which is return by API server to Time object.
+      #
+      # @return [Time]
+      #
+      def modified_on
+        Time.at(@modified_on)
+      end
+
+      #
+      # Adds field to datasource.
+      #
+      # @param [GroupDocs::Assembly::DataSource::Field] question
+      # @raise [ArgumentError] if field is not GroupDocs::Assembly::DataSource::Field object
+      #
+      def add_field(field)
+        field.is_a?(GroupDocs::Assembly::DataSource::Field) or raise ArgumentError,
+          "Field should be GroupDocs::Assembly::DataSource::Field object, received: #{field.inspect}"
+
+        @fields ||= Array.new
+        @fields << field
+      end
 
     end # DataSource
   end # Assembly
--- spec/groupdocs/assembly/datasource_spec.rb
+++ spec/groupdocs/assembly/datasource_spec.rb
@@ -4,6 +4,40 @@ describe GroupDocs::Assembly::DataSource do
 
   it_behaves_like GroupDocs::Api::Entity
 
-  it { should respond_to(:id)       }
-  it { should respond_to(:id=)      }
+  it { should respond_to(:id)                }
+  it { should respond_to(:id=)               }
+  it { should respond_to(:questionnaire_id)  }
+  it { should respond_to(:questionnaire_id=) }
+  it { should respond_to(:created_on)        }
+  it { should respond_to(:created_on=)       }
+  it { should respond_to(:modified_on)       }
+  it { should respond_to(:modified_on=)      }
+  it { should respond_to(:fields)            }
+  it { should respond_to(:fields=)           }
+
+  describe '#created_on' do
+    it 'returns converted to Time object Unix timestamp' do
+      subject.created_on = 1332950825
+      subject.created_on.should be_a(Time)
+    end
+  end
+
+  describe '#modified_on' do
+    it 'returns converted to Time object Unix timestamp' do
+      subject.modified_on = 1332950825
+      subject.modified_on.should be_a(Time)
+    end
+  end
+
+  describe '#add_field' do
+    it 'raises error if field is not GroupDocs::Assembly::DataSource::Field object' do
+      -> { subject.add_field('Field') }.should raise_error(ArgumentError)
+    end
+
+    it 'saves field' do
+      field = GroupDocs::Assembly::DataSource::Field.new
+      subject.add_field(field)
+      subject.fields.should == [field]
+    end
+  end
 end
