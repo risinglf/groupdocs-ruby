f6cadc090a8ff301c518eea4995ef5b9899d784f
Alex Rodionov <p0deje@gmail.com>
2012-04-05 22:44:29 +0700

    Not needed

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,3 @@
-# encoding: utf-8
-$LOAD_PATH.unshift(File.dirname(__FILE__))
-$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
-
 require 'simplecov'
 SimpleCov.configure do
   add_filter('spec/')
