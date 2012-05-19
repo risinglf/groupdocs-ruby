516917ba39e40626e2118233421c6644ebd20c3b
Alex Rodionov <p0deje@gmail.com>
2012-05-19 21:03:55 +0700

    More accessors for Rectangle. It's needed for Entity#inspect

--- lib/groupdocs/document/rectangle.rb
+++ lib/groupdocs/document/rectangle.rb
@@ -11,14 +11,16 @@ module GroupDocs
     attr_accessor :Height
 
     # Human-readable accessors
-    alias_method :x,  :X
-    alias_method :x=, :X=
-    alias_method :y,  :Y
-    alias_method :y=, :Y=
-    alias_method :w,  :Width
-    alias_method :w=, :Width=
-    alias_method :h,  :Height
-    alias_method :h=, :Height=
+    alias_method :x,      :X
+    alias_method :x=,     :X=
+    alias_method :y,      :Y
+    alias_method :y=,     :Y=
+    alias_method :w,      :Width
+    alias_method :w=,     :Width=
+    alias_method :width,  :w
+    alias_method :h,      :Height
+    alias_method :h=,     :Height=
+    alias_method :height, :Height
 
   end # Document::Rectangle
 end # GroupDocs
