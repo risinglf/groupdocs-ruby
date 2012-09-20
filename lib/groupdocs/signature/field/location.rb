7531e576aab90f9540f8c922bcee0cb828711f7f
Alex Rodionov <p0deje@gmail.com>
2012-09-20 16:07:19 +0700

    added Signature::Field::Location

---
+++ lib/groupdocs/signature/field/location.rb
@@ -0,0 +1,64 @@
+module GroupDocs
+  class Signature::Field::Location < Api::Entity
+
+    # @attr [String] id
+    attr_accessor :id
+    # @attr [String] documentId
+    attr_accessor :documentId
+    # @attr [String] fieldId
+    attr_accessor :fieldId
+    # @attr [Integer] page
+    attr_accessor :page
+    # @attr [Float] locationX
+    attr_accessor :locationX
+    # @attr [Float] locationY
+    attr_accessor :locationY
+    # @attr [Float] locationWidth
+    attr_accessor :locationWidth
+    # @attr [Float] locationHeight
+    attr_accessor :locationHeight
+    # @attr [String] fontName
+    attr_accessor :fontName
+    # @attr [String] fontColor
+    attr_accessor :fontColor
+    # @attr [Integer] fontSize
+    attr_accessor :fontSize
+    # @attr [Boolean] fontBold
+    attr_accessor :fontBold
+    # @attr [Boolean] fontItalic
+    attr_accessor :fontItalic
+    # @attr [Boolean] fontUnderline
+    attr_accessor :fontUnderline
+
+    # Human-readable accessors
+    alias_method :document_id,      :documentId
+    alias_method :document_id=,     :documentId=
+    alias_method :field_id,         :fieldId
+    alias_method :field_id=,        :fieldId=
+    alias_method :location_x,       :locationX
+    alias_method :location_x=,      :locationX=
+    alias_method :location_y,       :locationY
+    alias_method :location_y=,      :locationY=
+    alias_method :location_w,       :locationWidth
+    alias_method :location_w=,      :locationWidth=
+    alias_method :location_width,   :locationWidth
+    alias_method :location_width=,  :locationWidth=
+    alias_method :location_h,       :locationHeight
+    alias_method :location_h=,      :locationHeight=
+    alias_method :location_height,  :locationHeight
+    alias_method :location_height=, :locationHeight=
+    alias_method :font_name,        :fontName
+    alias_method :font_name=,       :fontName=
+    alias_method :font_color,       :fontColor
+    alias_method :font_color=,      :fontColor=
+    alias_method :font_size,        :fontSize
+    alias_method :font_size=,       :fontSize=
+    alias_method :font_bold,        :fontBold
+    alias_method :font_bold=,       :fontBold=
+    alias_method :font_italic,      :fontItalic
+    alias_method :font_italic=,     :fontItalic=
+    alias_method :font_underline,   :fontUnderline
+    alias_method :font_underline=,  :fontUnderline=
+
+  end # Signature::Field::Location
+end # GroupDocs
