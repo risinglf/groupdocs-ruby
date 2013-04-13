f26385f9874ab985ef6eace38c4905db5b1a320e
Alex Rodionov <p0deje@gmail.com>
2013-04-13 19:25:48 +0700

    Role#can* methods are now boolean

--- spec/groupdocs/signature/role_spec.rb
+++ spec/groupdocs/signature/role_spec.rb
@@ -40,51 +40,8 @@ describe GroupDocs::Signature::Role do
   it { should alias_accessor(:can_annotate, :canAnnotate) }
   it { should alias_accessor(:can_delegate, :canDelegate) }
 
-  describe '#can_edit?' do
-    it 'returns true if role can edit' do
-      subject.can_edit = 1
-      subject.can_edit?.should be_true
-    end
-
-    it 'returns false if role cannot edit' do
-      subject.can_edit = 0
-      subject.can_edit?.should be_false
-    end
-  end
-
-  describe '#can_sign?' do
-    it 'returns true if role can sign' do
-      subject.can_sign = 1
-      subject.can_sign?.should be_true
-    end
-
-    it 'returns false if role cannot sign' do
-      subject.can_sign = 0
-      subject.can_sign?.should be_false
-    end
-  end
-
-  describe '#can_annotate?' do
-    it 'returns true if role can annotate' do
-      subject.can_annotate = 1
-      subject.can_annotate?.should be_true
-    end
-
-    it 'returns false if role cannot annotate' do
-      subject.can_annotate = 0
-      subject.can_annotate?.should be_false
-    end
-  end
-
-  describe '#can_delegate?' do
-    it 'returns true if role can delegate' do
-      subject.can_delegate = 1
-      subject.can_delegate?.should be_true
-    end
-
-    it 'returns false if role cannot delegate' do
-      subject.can_delegate = 0
-      subject.can_delegate?.should be_false
-    end
-  end
+  it { should have_alias(:can_edit?,     :can_edit)     }
+  it { should have_alias(:can_sign?,     :can_sign)     }
+  it { should have_alias(:can_annotate?, :can_annotate) }
+  it { should have_alias(:can_delegate?, :can_delegate) }
 end
