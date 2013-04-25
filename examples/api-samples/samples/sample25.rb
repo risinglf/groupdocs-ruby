1ec90986fe2c0b23fefceb15420edcddc447f7e1
averjr <averkiev.jr@gmail.com>
2013-04-25 08:26:48 +0300

    Fix Samples list. Add GUID providing methods in 25th sample.

--- examples/api-samples/samples/sample25.rb
+++ examples/api-samples/samples/sample25.rb
@@ -8,7 +8,10 @@ post '/sample25' do
   # set variables
   set :client_id, params[:client_id]
   set :private_key, params[:private_key]
-  
+  set :source, params[:source]
+  set :file_id, params[:fileId]
+  set :url, params[:url]
+
   # Set download path
   downloads_path = "#{File.dirname(__FILE__)}/../public/downloads"
 
@@ -20,37 +23,61 @@ post '/sample25' do
   end
 
   begin
-    # check required variables
+    # Check required variables
     raise 'Please enter all required parameters' if settings.client_id.empty? or settings.private_key.empty?
 
-    # construct path
-    file_path = "#{Dir.tmpdir}/#{params[:file][:filename]}"
-    # open file
-    File.open(file_path, 'wb') { |f| f.write(params[:file][:tempfile].read) }
-    # make a request to API using client_id and private_key
-    file = GroupDocs::Storage::File.upload!(file_path, {}, {:client_id => settings.client_id, :private_key => settings.private_key})
 
+    # get document by file GUID
+    case settings.source
+      when 'guid'
+        # Create instance of File
+        file = GroupDocs::Storage::File.new({:guid => settings.file_id})
+      when 'local'
+        # Construct path
+        file_path = "#{Dir.tmpdir}/#{params[:file][:filename]}"
+        # Open file
+        File.open(file_path, 'wb') { |f| f.write(params[:file][:tempfile].read) }
+        # Make a request to API using client_id and private_key
+        file = GroupDocs::Storage::File.upload!(file_path, {}, {:client_id => settings.client_id, :private_key => settings.private_key})
+      when 'url'
+        # Upload file from defined url
+        file = GroupDocs::Storage::File.upload_web!(settings.url, {:client_id => settings.client_id, :private_key => settings.private_key})
+      else
+        raise 'Wrong GUID source.'
+    end
+
+    # Raise exception if something went wrong
+    raise 'No such file' unless file.is_a?(GroupDocs::Storage::File)
+
+    # Make GroupDocs::Storage::Document instance
     document = file.to_document
 
-    # create datasource with fields
+    # Create datasource with fields
     datasource = GroupDocs::DataSource.new
 
+    # Get arry of document's fields
     fields = document.fields!({:client_id => settings.client_id, :private_key => settings.private_key})
 
+    # Create Field instance and fill the fields
     datasource.fields = fields.map { |field| GroupDocs::DataSource::Field.new(name: field.name, type: :text, values: %w(value1 value2)) }
 
+    # Adds datasource.
     datasource.add!({:client_id => settings.client_id, :private_key => settings.private_key})
 
 
-    # merge and convert
+    # Creates new job to merge datasource into document.
     job = document.datasource!(datasource, {:new_type => 'pdf'}, {:client_id => settings.client_id, :private_key => settings.private_key})
     sleep 10 # wait for merge and convert
 
+    # Returns an hash of input and output documents associated to job.
     document = job.documents!({:client_id => settings.client_id, :private_key => settings.private_key})
+
+    # Download file
     document[:inputs][0].outputs[0].download!("#{File.dirname(__FILE__)}/../public/downloads", {:client_id => settings.client_id, :private_key => settings.private_key})
 
-    # Get converted document GUID
+    # Set converted document GUID
     guid = document[:inputs][0].outputs[0].guid
+    # Set converted document Name
     file_name = document[:inputs][0].outputs[0].name
 
     # Set iframe with document GUID or raise an error
