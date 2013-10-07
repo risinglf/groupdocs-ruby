2c61a5d5f72dfd16eb56f6d08462f0357bbe2fba
Evgen <3skif9@mail.ru>
2013-10-07 08:22:44 +0300

    modified the sample 30

--- examples/api-samples/samples/sample30.rb
+++ examples/api-samples/samples/sample30.rb
@@ -8,17 +8,25 @@ post '/sample30' do
   # set variables
   set :client_id, params[:client_id]
   set :private_key, params[:private_key]
-  set :file_id, params[:fileId]
+  set :name, params[:name]
 
   begin
     # check required variables
-    raise 'Please enter all required parameters' if settings.client_id.empty? or settings.private_key.empty? or settings.file_id.empty?
+    raise 'Please enter all required parameters' if settings.client_id.empty? or settings.private_key.empty? or settings.name.empty?
 
-    # make a request to API using client_id and private_key
-    file = GroupDocs::Storage::File.new({:guid => settings.file_id})
+    # get List files from GroupDocs Storage
+    file = GroupDocs::Storage::Folder.list!("", {}, {:client_id => settings.client_id, :private_key => settings.private_key})
+    file_name = ''
+
+    # choose the desired file
+    file.map do |element|
+      if element.name == settings.name
+          file_name = element
+      end
+    end
 
     # delete file from GroupDocs Storage
-    result = file.delete!({:client_id => settings.client_id, :private_key => settings.private_key})
+    file_name.delete!({:client_id => settings.client_id, :private_key => settings.private_key})
 
     message = 'File was deleted from GroupDocs Storage'
 
@@ -27,5 +35,5 @@ post '/sample30' do
   end
 
   # set variables for template
-  haml :sample30, :locals => {:userId => settings.client_id, :privateKey => settings.private_key, :message => message, :fileId => settings.file_id, :err => err}
+  haml :sample30, :locals => {:userId => settings.client_id, :privateKey => settings.private_key, :message => message, :name => settings.name, :err => err}
 end
