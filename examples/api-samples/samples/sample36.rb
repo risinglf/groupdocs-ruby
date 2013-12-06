bc592a935615ca8641999ae0d29f77d28eeede93
Evgen <3skif9@mail.ru>
2013-12-06 14:56:17 +0200

    create new sample36

---
+++ examples/api-samples/samples/sample36.rb
@@ -0,0 +1,57 @@
+# GET request
+get '/sample36' do
+  haml :sample36
+end
+
+
+
+
+# POST request
+post '/sample36' do
+  # Set variables
+  set :client_id, params[:clientId]
+  set :private_key, params[:privateKey]
+  set :envelope, params[:envelopeGuid]
+  set :base_path, params[:basePath]
+
+  # Set download path
+  downloads_path = "#{File.dirname(__FILE__)}/../public/downloads"
+
+  # Remove all files from download directory or create folder if it not there
+  if File.directory?(downloads_path)
+    Dir.foreach(downloads_path) { |f| fn = File.join(downloads_path, f); File.delete(fn) if f != '.' && f != '..' }
+  else
+    Dir::mkdir(downloads_path)
+  end
+
+
+  begin
+    # Check required variables
+    raise 'Please enter all required parameters' if settings.client_id.empty? or settings.private_key.empty?
+
+    if settings.base_path.empty? then settings.base_path = 'https://api.groupdocs.com' end
+
+    # Configure your access to API server
+    GroupDocs.configure do |groupdocs|
+      groupdocs.client_id = settings.client_id
+      groupdocs.private_key = settings.private_key
+      # Optionally specify API server and version
+      groupdocs.api_server = settings.base_path # default is 'https://api.groupdocs.com'
+    end
+
+    # Get envelope from GroupDocs Storage
+    envelope = GroupDocs::Signature::Envelope.get!(settings.envelope)
+
+    #Download signed files
+    path = envelope.signed_documents!(downloads_path)
+
+    #Get file name
+    file = path.split('/').last
+
+  rescue Exception => e
+    err = e.message
+  end
+
+  # set variables for template
+  haml :sample36, :locals => {:userId => settings.client_id, :privateKey => settings.private_key, :fileName => file, :err => err}
+end
